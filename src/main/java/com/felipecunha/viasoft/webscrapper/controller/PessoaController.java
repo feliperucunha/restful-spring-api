7832499ab296024607226c6cb69c99bbba7f3141
Felipe Cunha <feliperubenmv@gmail.com>
2020-10-02 19:15:50 -0300

    Delete method

--- src/main/java/com/felipecunha/viasoft/webscrapper/controller/PessoaController.java
+++ src/main/java/com/felipecunha/viasoft/webscrapper/controller/PessoaController.java
@@ -4,6 +4,7 @@ import com.felipecunha.viasoft.webscrapper.controller.dto.PessoaRq;
 import com.felipecunha.viasoft.webscrapper.controller.dto.PessoaRs;
 import com.felipecunha.viasoft.webscrapper.model.Pessoa;
 import com.felipecunha.viasoft.webscrapper.repository.PessoaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class PessoaController {
         this.pessoaRepository = pessoaRepository;
     }
 
+    //GET
     @GetMapping("/")
     public List<PessoaRs> findAll() {
         var pessoas = pessoaRepository.findAll();
@@ -28,17 +30,47 @@ public class PessoaController {
                 .collect(Collectors.toList());
     }
 
+    //GET
     @GetMapping("/{id}")
     public PessoaRs findById(@PathVariable("id") Long id) {
         var pessoa = pessoaRepository.getOne(id);
         return PessoaRs.converter(pessoa);
     }
 
+    //POST
     @PostMapping("/")
     public void salvarPessoa(@RequestBody PessoaRq pessoa) {
         var p = new Pessoa();
         p.setNome(pessoa.getNome());
         p.setSobrenome(pessoa.getSobrenome());
         pessoaRepository.save(p);
+    }
+
+    //DELETE
+    @DeleteMapping("/{id}")
+    public HttpStatus apagarPessoa(@PathVariable("id") Long id) throws Exception {
+         var p = pessoaRepository.findById(id);
+         try {
+             pessoaRepository.deleteById(id);
+         } catch(Exception e) {
+            throw new Exception("Pessoa não encontrada");
+        }
+         return HttpStatus.OK;
+
+    }
+
+    //PUT
+    @PutMapping("/{id}")
+    public void atualizarPessoa(@PathVariable("id") Long id, @RequestBody PessoaRq pessoa) throws Exception {
+        var p = pessoaRepository.findById(id);
+
+        if (p.isPresent()) {
+            var pessoaSave = p.get();
+            pessoaSave.setNome(pessoa.getNome());
+            pessoaSave.setSobrenome(pessoa.getSobrenome());
+            pessoaRepository.save(pessoaSave);
+        } else {
+            throw new Exception("Pessoa não encontrada");
+        }
     }
 }
